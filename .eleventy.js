fc4e913ff289c2fd472fea4c6f47bf14ef7b0af8
Katsushi OUGI <ougi@to-r.net>
2022-06-19 21:44:13 +0900

    scssビルド追加

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,13 @@
 module.exports = (config) => {
   // Pass-through files
-  config.addPassthroughCopy('src/assets');
+  config.addPassthroughCopy('src/assets/img/');
+  config.addPassthroughCopy('src/assets/css/');
+  config.addPassthroughCopy('src/assets/js/');
+
+  // watch
+  config.addWatchTarget("./src/assets/scss/");
 
   return {
-    dir: { input: 'src', output: '_site', includes: 'includes' }
+    dir: { input: 'src', output: 'site', includes: 'includes' }
   }
 }
