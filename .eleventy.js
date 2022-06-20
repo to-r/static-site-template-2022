ba370bcf74ee8d395bca4099591287a02c10e8aa
Katsushi OUGI <ougi@to-r.net>
2022-06-20 23:09:52 +0900

    pictureタグのショートコード実装

--- .eleventy.js
+++ .eleventy.js
@@ -13,6 +13,32 @@ module.exports = (config) => {
     autoescape: false, // warning: don’t do this!
   });
 
+  /**
+   * picture要素のショートコード
+   * usage:
+   * {% picture_tag '/assets/img/hoge.png' %}
+   * {% picture_tag '/assets/img/hoge.png', 'ダミー画像', true %}
+   */
+  config.addNunjucksShortcode(
+    "picture_tag",
+    (filename, alt = "", useRetina = false) => {
+      const basename = filename.substring(0, filename.lastIndexOf("."));
+      const ext = filename.substring(filename.lastIndexOf("."));
+      const pcFileNames = [filename].concat(
+        useRetina ? [`${basename}_@2x${ext}`] : []
+      );
+
+      const spFileName = `${basename}_sp${ext}`;
+
+      return `
+      <picture>
+        <source media="(min-width: 751px)" srcset="${pcFileNames.join(",")}">
+        <source media="(max-width: 750px)" srcset="${spFileName}">
+        <img src="${pcFileNames[0]}" alt="${alt}">
+      </picture>`;
+    }
+  );
+
   return {
     dir: { input: "src", output: "site", includes: "_includes" },
   };
